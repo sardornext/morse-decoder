46247dc7488575163034c139013ff4f88a0dba6c
Sardor <139249785+sardornext@users.noreply.github.com>
2024-08-05 12:29:54 +0500

    feat: test for Morse code decoder

--- src/index.js
+++ src/index.js
@@ -1,46 +1,93 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let morse = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        let letter = expr.slice(i, i + 10);
+        if (letter === '**********') {
+            result += ' ';
+        } else {
+            for (let j = 0; j < letter.length; j += 2) {
+                let symbol = letter.slice(j, j + 2);
+                if (symbol === '10') {
+                    morse += '.';
+                } else if (symbol === '11') {
+                    morse += '-';
+                }
+            }
+            result += MORSE_TABLE[morse];
+            morse = '';
+        }
+    }
+    return result;
 }
 
+// function decode(expr) {
+//      const morseWords = expr.split('   ');
+//      const decodedMessage = morseWords.map(word => {
+//          return word.split(' ').map(char => MORSE_TABLE[char]).join(' ');
+//      }).join(' ');
+//      return decodedMessage;
+// }
+
+// function decode(expr) {
+//   let result = '';
+//   for (let i = 0; i < expr.length; i += 10) {
+//     const chunk = expr.slice(i, i + 10);
+//     if (chunk === '**********') {
+//       result += ' ';
+//       continue;
+//     }
+//     let morseCode = '';
+//     for (let j = 0; j < 10; j += 2) {
+//       const pair = chunk.slice(j, j + 2);
+//       if (pair === '10') morseCode += '.';
+//       else if (pair === '11') morseCode += '-';
+//     }
+//     result += MORSE_TABLE[morseCode];
+//   }
+
+//   return result;
+// }
+
 module.exports = {
-    decode
-}
+  decode,
+};
